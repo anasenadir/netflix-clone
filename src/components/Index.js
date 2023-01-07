e342e6218191a978de06f986a504ba07bc23fc1d
anasenadire <enadireahmed@gmail.com>
2023-01-07 23:14:43 +0000

    create a Row component,and create a helper folder and put the axios and requests files on it. Just an organizational purpose

---
+++ src/components/Index.js
@@ -0,0 +1,27 @@
+import Row from "./Row/Row"
+import requests from "../helpers/requests"
+import { Banner } from "./Banner/Banner";
+
+const Index = ()=>{
+    return (
+        <div>
+            {/* <Banner / > */}
+            <Row  
+            title="Tranding " 
+            fetchURL={requests.fetchTrending}
+            isLargeRow
+            />
+            <Row  title="Action Movies " fetchURL={requests.fetchActionMovies}/>
+            <Row  title="Comedy Movies " fetchURL={requests.fetchComedyMovies}/>
+            <Row  title="Horro Movies" fetchURL={requests.fetchHorrorMovies}/>
+            <Row  title="Documentaries" fetchURL={requests.fetchDocumentaries}/>
+            <Row  title="Netflix Originals" fetchURL={requests.fetchNetflixOriginals}/>
+            <Row  title="Romantice Movies" fetchURL={requests.fetchRomanceMovies}/>
+            <Row  title="Top Rated" fetchURL={requests.fetchTopRated}/>
+        </div>
+    );
+}
+
+
+
+export default Index;
