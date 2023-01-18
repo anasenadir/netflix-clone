76da47b7c5f923ff4cf8b0810312210c642b9cd4
anasenadire <enadireahmed@gmail.com>
2023-01-18 01:44:03 +0000

    switching between showing sign in button and The Profile Avatar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,9 +3,12 @@ import logo from '../../assets/Logonetflix.png';
 import logoAvatar from "../../assets/Netflix-avatar.png"
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const Navbar = ()=>{
     const [navState , setNavState] = useState(false);
+    const usersLoginState = useSelector(state => state.user.user); 
+
     useEffect(()=>{
         window.addEventListener('scroll' , ()=>{
             if(document.documentElement.scrollTop > 100)
@@ -22,17 +25,28 @@ const Navbar = ()=>{
     }, [])
 
     return <div className={`nav ${navState && 'show'}`}>
-            <Link to='/'>
-                <img className='nav__logo' src={logo} alt="" />
-            </Link>
+            {
+                usersLoginState ? 
+                <Link to='/'>
+                    <img className='nav__logo' src={logo} alt="" />
+                </Link>
+                :
+                <Link to='/signup'>
+                    <img className='nav__logo' src={logo} alt="" />
+                </Link>
+            }
             <div className='nav__links_container'>
+                {
+                !usersLoginState &&
                 <button className='nav__signin__button'>
                     <Link to='/signin'>
                         Sign in
                     </Link>
                 </button>
-                <Link to="/signin">
-                    <img className='nav__login' src={logoAvatar} alt="" />
+                }
+                
+                <Link to="/profile">
+                    {usersLoginState && <img className='nav__login' src={logoAvatar} alt="" />}
                 </Link>
             </div>
         </div>
