6d21b77b80ce7bc857573e1b052854a131d95605
anasenadire <enadireahmed@gmail.com>
2023-01-18 01:36:12 +0000

    I've put the (onAuthStateChange) to chick on every render if the use still logged in or not

--- src/App.js
+++ src/App.js
@@ -4,8 +4,25 @@ import Signin from "./pages/Signin/Signin";
 import Signup from "./pages/Signup/Signup";
 import Profile from "./pages/profile/Profile";
 import { Link , Route , Routes ,  BrowserRouter} from "react-router-dom";
+
+import { auth , onAuthStateChanged} from "./helpers/firebase";
+import { loginUser , logoutUser } from "./helpers/store/slices/userSlice";
+import { useDispatch } from "react-redux";
+
+
 import './App.css'
 function App() {
+  const dispatchUserState = useDispatch();
+  onAuthStateChanged(auth , user=>{
+        if(user){
+            console.log('login');
+            dispatchUserState(loginUser(user.email));
+        }
+        else{
+            console.log('logout');
+            dispatchUserState(logoutUser());
+        }
+    })
   return (
     <div className="App">
       <BrowserRouter>
