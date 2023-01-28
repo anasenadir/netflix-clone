8aca14ddc30e47476abe1f355f87f6ecccf09d20
anasenadire <enadireahmed@gmail.com>
2023-01-28 02:55:32 +0000

    make this component responcive, add the outline effect when the user choose a click on a movie

--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -5,7 +5,7 @@ import YouTube from "react-youtube";
 import movieTrailer from "movie-trailer";
 
 // icons
-import { AiOutlineHeart ,AiTwotoneHeart } from "react-icons/ai";
+import { AiOutlineHeart ,AiTwotoneHeart , AiFillCaretDown } from "react-icons/ai";
 import { FcLike } from "react-icons/fc";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -16,10 +16,13 @@ const Row = ({title , fetchURL , isLargeRow  ,globalIndexTrailer ,setGlobalIndex
     const [movies , setMovies] = useState([]);
     const [localRowTrailer , setlocalRowTrailer]  = useState(''); 
     const [trailerURL , setTrailerURL] = useState('');
+    // ======================
+    const [isFavoriteIconClicked  , setFavoriteClicked] = useState(false); 
 
     useEffect( () => {
         async function fetchData(){
             let response = await axios.get(fetchURL);
+            
             setMovies(response.data.results);
         }
         fetchData();
@@ -35,8 +38,12 @@ const Row = ({title , fetchURL , isLargeRow  ,globalIndexTrailer ,setGlobalIndex
             setTrailerURL(globalIndexTrailer);
         }
     } , [globalIndexTrailer ])
+    
+    
+    const handlePlayFunc = (e , movie)=>{
+        // show the outline by default
+        activeMovie(e , true);
 
-    const handlePlayFunc = (movie)=>{
         // search for a movie by name if name is exists
         // dispatch(setMovieTr(''));
         movieTrailer(movie?.name ||movie?.title || movie?.original_title || "").then((url)=>{
@@ -63,15 +70,38 @@ const Row = ({title , fetchURL , isLargeRow  ,globalIndexTrailer ,setGlobalIndex
             const urlParams = new URLSearchParams(new URL(url).search);
             // dispatch(setMovieTr(urlParams.get('v')));
             if(urlParams.get('v') == trailerURL){
+                // hide the outline 
+                activeMovie(e , false);
+                
                 setGlobalIndexTrailer('')
                 setlocalRowTrailer('')
             }else{
-                setGlobalIndexTrailer(urlParams.get('v'))
-                setlocalRowTrailer(urlParams.get('v'))
+                // show the outline
+                activeMovie(e , true);
+
+
+                setGlobalIndexTrailer(urlParams.get('v'));
+                setlocalRowTrailer(urlParams.get('v'));
             }
-        }).catch((e)=>console.log(e));
+        }).catch((e)=>{
+            // hide the outline 
+            activeMovie(e , false);
+        });
+
+        
     }
 
+    // this function is for adding the outline when the user choosing a movie
+    const activeMovie = (e , isNotTheSame) => {
+        let movies = document.querySelectorAll('.active');
+        // console.log(movies);
+        movies.forEach((elem)=>{
+            elem.classList.remove('active')
+        })
+        if(isNotTheSame){
+            !e.target.offsetParent.classList.contains('active') ? e.target.offsetParent.classList.add('active') : e.target.offsetParent.classList.remove('active') ;
+        }
+    }
 
     let opts = {
         height: 350,
@@ -84,30 +114,40 @@ const Row = ({title , fetchURL , isLargeRow  ,globalIndexTrailer ,setGlobalIndex
         <h3 className="row__title">{title}</h3>
         <Swiper 
                 spaceBetween={10}
-                slidesPerView={isLargeRow ? 7.8 : 6}
+                slidesPerView={3}
+                breakpoints={{
+                    768: {
+                        slidesPerView: 4,
+                    },
+                    900: {
+                        slidesPerView: 6,
+                    },
+                    1200: {
+                        slidesPerView: 8,
+                    },
+                    1600:{
+                        slidesPerView: 10
+                    }
+                }}
                 >
                 {movies.map((movie) =>{
                     // return 
                     if(movie.backdrop_path){
-                    return <SwiperSlide  key={movie.id} className="poster__wrapper">
+                    return <SwiperSlide  key={movie.id} className="poster__wrapper" onClick={(e)=>{handlePlayFunc(e , movie)}}>
 
-                        <div className="poster__favorites">
-                            <AiOutlineHeart className="heart default__heart"/>
-                            <FcLike  className="heart"/>
-                        </div>
                         <img 
                         key={movie.id}
                         // className={isLargeRow ? 'row__poster largRow' : 'row__poster'}
                         className={`row__poster ${isLargeRow && 'largRow'}`}
                         src={isLargeRow ? `${base_img_url}${movie.poster_path}` : `${base_img_url}${movie.backdrop_path}`} 
                         alt={movie.title}
-                        onClick={()=>{handlePlayFunc(movie)}}
                         />
+                        <AiFillCaretDown className="caret__down"/>
                     </SwiperSlide>
                     }
                 })}
+        {trailerURL && <YouTube videoId={trailerURL} className="youtube__video" opts={opts} />}
         </Swiper>
-        {trailerURL && <YouTube videoId={trailerURL} style={{margin:"0 25px"}} opts={opts} />}
     </>
 }
 
